69d9e90a4eb0df230c56f0d2702b4a7e731cb560
Jiri Otoupal <jiri.otoupal@uptim.ai>
2021-05-02 19:24:39 +0200

    New Version

--- pycrosskit/__init__.py
+++ pycrosskit/__init__.py
@@ -2,6 +2,35 @@
 pycrosskit
 ~~~~~~
 
-The pycrosskit package - a Python package template project that is intended
-to be used as a cookie-cutter for developing new Python packages.
+The pycrosskit package - a Python package that makes Shortcuts and Environment Variables Easy
 """
+
+#### Shortcuts usage:
+
+
+# Will Create shortcut
+# * at Desktop if desktop is True
+# * at Start Menu if start_menu is True
+#
+#   Shortcut(shortcut_name, exec_path, description,
+#         icon_path, desktop, start_menu)
+#
+## Will Delete shortcut
+## * at Desktop if desktop is True
+## * at Start Menu if start_menu is True
+#   Shortcut.delete(shortcut_name, desktop, start_menu)
+#
+#
+#
+##### Environment Variables usage:
+#
+#
+## Will Set Persistent Value for Variable in System
+## * subkey works only for windows like file in folder
+## * reg_path works only for windows as register path
+#   SysEnv.set_var(name, value, subkey, reg_path=default_reg_path)
+
+## Will Get Persistent Value for Variable in System
+## * reg_path works only for windows as register path
+## * delete, deletes key from environment and its subkeys after read
+#   SysEnv.get_var(name, reg_path=default_reg_path, delete=False)
