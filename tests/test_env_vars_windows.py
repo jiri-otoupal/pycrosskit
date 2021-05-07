7d847615a8e09ca6359166160d8dba79c328cb96
Jiri Otoupal <jiri.otoupal@uptim.ai>
2021-05-07 16:46:44 +0200

    Win tests fix

--- tests/test_env_vars_windows.py
+++ tests/test_env_vars_windows.py
@@ -6,23 +6,27 @@ from pycrosskit.shortcuts import Shortcut
 
 class WindowsVarsTest(unittest.TestCase):
     def test_set_var(self):
-        try:
-            SysEnv.set_var("test", "test", registry=False)
-        except:
-            self.fail("")
+        if Shortcut.get_platform() == "win":
+            try:
+                SysEnv.set_var("test", "test", registry=False)
+            except:
+                self.fail("")
 
     def test_get_var(self):
-        SysEnv.set_var("test", "test")
-        self.assertEqual(SysEnv.get_var("test", registry=False), "test")
+        if Shortcut.get_platform() == "win":
+            SysEnv.set_var("test", "test")
+            self.assertEqual(SysEnv.get_var("test", registry=False), "test")
 
     def test_get_var_remove(self):
-        SysEnv.set_var("test", "test", registry=False)
-        self.assertTrue(SysEnv.get_var("test", registry=False, delete=True), "test")
+        if Shortcut.get_platform() == "win":
+            SysEnv.set_var("test", "test", registry=False)
+            self.assertTrue(SysEnv.get_var("test", registry=False, delete=True), "test")
 
     def test_get_var_remove_fail(self):
-        SysEnv.set_var("test", "test", registry=True)
-        with self.assertRaises(FileNotFoundError):
-            SysEnv.get_var("test", registry=False, delete=True)
+        if Shortcut.get_platform() == "win":
+            SysEnv.set_var("test", "test", registry=True)
+            with self.assertRaises(FileNotFoundError):
+                SysEnv.get_var("test", registry=False, delete=True)
 
 
 if __name__ == '__main__':
