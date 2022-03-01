4793c69bded086bd7f9330370722f86a5adcc377
jiri_otoupal <Lkw2Rn89p7yNJUMRD4fv>
2022-03-01 18:52:10 +0100

    Test update

--- tests/test_shortcut.py
+++ tests/test_shortcut.py
@@ -7,48 +7,30 @@ from pycrosskit.shortcuts import Shortcut
 class Test_Shortcuts(unittest.TestCase):
 
     def test_create_desktop(self):
-        try:
-            sh = Shortcut("Test", "__init__.py", desktop=True)
-            self.assertEqual(True, os.path.exists(sh.desktop_path))
-        except:
-            self.assertEqual(True, False)
+        sh = Shortcut("Test", "__init__.py", desktop=True)
+        self.assertEqual(True, os.path.exists(sh.desktop_path))
 
     def test_delete_desktop(self):
-        try:
-            desktop, startmenu = Shortcut.delete("Test", desktop=True)
-            self.assertEqual(True, not os.path.exists(desktop))
-        except:
-            self.assertEqual(True, False)
+        desktop, startmenu = Shortcut.delete("Test", desktop=True)
+        self.assertEqual(True, not os.path.exists(desktop))
 
     def test_create_startmenu(self):
-        try:
-            sh = Shortcut("Test", "__init__.py", start_menu=True)
-            self.assertEqual(True, os.path.exists(sh.startmenu_path))
-        except:
-            self.assertEqual(True, False)
+        sh = Shortcut("Test", "__init__.py", start_menu=True)
+        self.assertEqual(True, os.path.exists(sh.startmenu_path))
 
     def test_delete_startmenu(self):
-        try:
-            desktop, startmenu = Shortcut.delete("Test", start_menu=True)
-            self.assertEqual(True, not os.path.exists(startmenu))
-        except:
-            self.assertEqual(True, False)
+        desktop, startmenu = Shortcut.delete("Test", start_menu=True)
+        self.assertEqual(True, not os.path.exists(startmenu))
 
     def test_create_both(self):
-        try:
-            sh = Shortcut("Test", "__init__.py", desktop=True, start_menu=True)
-            self.assertEqual(True, os.path.exists(sh.desktop_path))
-            self.assertEqual(True, os.path.exists(sh.startmenu_path))
-        except:
-            self.assertEqual(True, False)
+        sh = Shortcut("Test", "__init__.py", desktop=True, start_menu=True)
+        self.assertEqual(True, os.path.exists(sh.desktop_path))
+        self.assertEqual(True, os.path.exists(sh.startmenu_path))
 
     def test_delete_both(self):
-        try:
-            desktop, startmenu = Shortcut.delete("Test", desktop=True, start_menu=True)
-            self.assertEqual(True, not os.path.exists(desktop))
-            self.assertEqual(True, not os.path.exists(startmenu))
-        except:
-            self.assertEqual(True, False)
+        desktop, startmenu = Shortcut.delete("Test", desktop=True, start_menu=True)
+        self.assertEqual(True, not os.path.exists(desktop))
+        self.assertEqual(True, not os.path.exists(startmenu))
 
 
 if __name__ == '__main__':
