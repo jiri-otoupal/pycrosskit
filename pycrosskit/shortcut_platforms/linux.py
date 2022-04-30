fa8a49377ba9d91bfe564754813b254d2cc1c245
jiri_otoupal <Lkw2Rn89p7yNJUMRD4fv>
2022-04-30 14:41:41 +0200

    added doc

--- pycrosskit/shortcut_platforms/linux.py
+++ pycrosskit/shortcut_platforms/linux.py
@@ -85,18 +85,18 @@ def create_shortcut(shortcut_instance,
     :rtype: str, str
     """
     if shortcut_instance.work_path is None:
-        text = DESKTOP_FORM.format(name=shortcut_instance.shortcut_name,
-                                   desc=shortcut_instance.description,
-                                   exe=shortcut_instance.exec_path,
-                                   icon=shortcut_instance.icon_path,
-                                   args=shortcut_instance.arguments)
+        file_content = DESKTOP_FORM.format(name=shortcut_instance.shortcut_name,
+                                           desc=shortcut_instance.description,
+                                           exe=shortcut_instance.exec_path,
+                                           icon=shortcut_instance.icon_path,
+                                           args=shortcut_instance.arguments)
     else:
-        text = DESKTOP_FORM.format(name=shortcut_instance.shortcut_name,
-                                   desc=shortcut_instance.description,
-                                   exe="bash -c 'cd " + shortcut_instance.work_path + " && "
-                                       + shortcut_instance.exec_path + "'",
-                                   icon=shortcut_instance.icon_path,
-                                   args=shortcut_instance.arguments)
+        file_content = DESKTOP_FORM.format(name=shortcut_instance.shortcut_name,
+                                           desc=shortcut_instance.description,
+                                           exe="bash -c 'cd " + shortcut_instance.work_path + " && "
+                                               + shortcut_instance.exec_path + "'",
+                                           icon=shortcut_instance.icon_path,
+                                           args=shortcut_instance.arguments)
     user_folders = get_folders()
     desktop_path = str(
         Path(user_folders.desktop) / (shortcut_instance.shortcut_name + scut_ext))
@@ -104,20 +104,26 @@ def create_shortcut(shortcut_instance,
         Path(user_folders.startmenu) / (shortcut_instance.shortcut_name + scut_ext))
 
     if desktop:
-        __write_shortcut(desktop_path, shortcut_instance, text)
+        __write_shortcut(desktop_path, shortcut_instance, file_content)
 
     if startmenu:
-        __write_shortcut(startmenu_path, shortcut_instance, text)
+        __write_shortcut(startmenu_path, shortcut_instance, file_content)
 
     return desktop_path, startmenu_path
 
 
-def __write_shortcut(dest_path, shortcut_instance, icon_content):
+def __write_shortcut(dest_path, shortcut_instance, file_content):
+    """
+    Writes shortcut content to destination
+    :param dest_path: Path where write file
+    :param shortcut_instance: Instance of shortcut that will be used
+    :param file_content: Content of future icon from DESKTOP_FORM.format(...)
+    """
     if not os.path.exists(dest_path):
         os.makedirs(dest_path)
     dest = str(Path(dest_path) / (shortcut_instance.shortcut_name + scut_ext))
     with open(dest, 'w') as f_out:
-        f_out.write(icon_content)
+        f_out.write(file_content)
     st = os.stat(dest)
     os.chmod(dest, st.st_mode | stat.S_IEXEC)
 
