b10da288ec6f90f3c399dbc76cd2b499398334b1
jiri_otoupal <Lkw2Rn89p7yNJUMRD4fv>
2022-04-30 17:29:51 +0200

    Version 1.0.6.4.1 tilda not being expanded

--- pycrosskit/env_platforms/linux.py
+++ pycrosskit/env_platforms/linux.py
@@ -65,7 +65,7 @@ class LinVar:
 
             replacement_lines.append(line)
 
-        with open(shell_file, "w") as f:
+        with open(os.path.expanduser(shell_file), "w") as f:
             f.writelines(replacement_lines)
 
         cls.logger.debug(f"Finished Unsetting system variable {key}")
