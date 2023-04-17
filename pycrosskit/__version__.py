fc107de4e77d0a025905293188a176f7539571ac
jiri_otoupal <Lkw2Rn89p7yNJUMRD4fv>
2023-04-18 01:11:35 +0200

    Build fix

--- pycrosskit/__version__.py
+++ pycrosskit/__version__.py
@@ -11,7 +11,7 @@ __description__ = (
     "variables and shortcuts easy "
 )
 
-__version__ = "1.6.9"
+__version__ = "1.7.0"
 __author__ = "Jiri Otoupal"
 __author_email__ = "jiri-otoupal@ips-database.eu"
 __license__ = "Apache 2.0"
