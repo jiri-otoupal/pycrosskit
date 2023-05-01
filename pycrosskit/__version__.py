6e8f225f292694c07671059cf36450aa8914cacf
jiri_otoupal <Lkw2Rn89p7yNJUMRD4fv>
2023-05-01 20:57:13 +0200

    Version Change

--- pycrosskit/__version__.py
+++ pycrosskit/__version__.py
@@ -11,7 +11,7 @@ __description__ = (
     "variables and shortcuts easy "
 )
 
-__version__ = "1.7.1"
+__version__ = "1.7.2"
 __author__ = "Jiri Otoupal"
 __author_email__ = "jiri-otoupal@ips-database.eu"
 __license__ = "Apache 2.0"
