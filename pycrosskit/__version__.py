444ccafe4f8e84e190f01a39a0bb403a54d1bb4a
Jiri Otoupal <69390758+jiri-otoupal@users.noreply.github.com>
2024-10-03 11:15:12 +0200

    Update __version__.py

--- pycrosskit/__version__.py
+++ pycrosskit/__version__.py
@@ -11,7 +11,7 @@ __description__ = (
     "variables and shortcuts easy "
 )
 
-__version__ = "1.7.4"
+__version__ = "1.7.5"
 __author__ = "Jiri Otoupal"
 __author_email__ = "jiri-otoupal@ips-database.eu"
 __license__ = "Apache 2.0"
